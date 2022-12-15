2cd74f2aebd77a404e59b196a9c75f710767f599
Максим <maksim.zaripov.06@bk.ru>
2022-12-15 14:46:52 +0400

    wregwtrgthwst

--- src/main/java/ru/fantasydestiny/fantasydestiny/world/World.java
+++ src/main/java/ru/fantasydestiny/fantasydestiny/world/World.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Vector;
+import java.io.*;
 
 public class World {
     private final List<Location> locations = new ArrayList<>();
@@ -17,5 +18,28 @@ public class World {
 
     public List<Location> getLocations() {
         return locations;
+    }
+    public void addLocation(Location L){locations.add(L);}
+
+    public void Save(String WorldName){
+        try {
+            FileOutputStream f = new FileOutputStream(WorldName);
+            ObjectOutputStream out = new ObjectOutputStream(f);
+            out.writeObject(this);
+            f.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void Get(String WorldName){
+        try {
+            FileInputStream f = new FileInputStream(WorldName);
+            ObjectInputStream in = new ObjectInputStream(f);
+            World w=(World) in.readObject();
+            this=w;
+            f.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
