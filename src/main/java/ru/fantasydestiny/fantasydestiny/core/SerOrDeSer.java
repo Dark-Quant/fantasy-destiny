3688d71869f6cbc98eeeecf4e4d1682620ff1344
Максим <maksim.zaripov.06@bk.ru>
2022-12-23 15:03:28 +0400

    wregwtrgthwst

--- src/main/java/ru/fantasydestiny/fantasydestiny/core/SerOrDeSer.java
+++ src/main/java/ru/fantasydestiny/fantasydestiny/core/SerOrDeSer.java
@@ -7,51 +7,67 @@ import java.util.Map;
 
 public class SerOrDeSer {
 
-    static List<Field> getFieldNames(Field[] fields) {
-        return List.of(fields);
+    private static Boolean isPrimitiveOrWrapper(Field field) {
+        return field.getType().isPrimitive() ||
+                field.getType().getSimpleName().equals("String") ||
+                field.getType().getSimpleName().equals("Enum");
     }
-    static  Boolean isPrimitiveOrWrapper(Field field){
-        if(field.getType().isPrimitive() ||
-                field.getType().getSimpleName().equals("String")||
-                field.getType().getSimpleName().equals("Integer")||
-                field.getType().getSimpleName().equals("Boolean")||
-                field.getType().getSimpleName().equals("Enum")||
-                field.getType().getSimpleName().equals("Double")||
-                field.getType().getSimpleName().equals("Float")||
-                field.getType().getSimpleName().equals("Long"))
-        {
-            return true;
+    static Boolean isPrimitiveOrWrapper(Object obj){
+        return  obj.getClass().isPrimitive();
+    }
+
+    private static List<?> convertObjectToList(Object obj) {
+        List<?> list = new ArrayList<>();
+        if (obj.getClass().isArray()) {
+            list = Arrays.asList((Object[])obj);
+        } else if (obj instanceof Collection) {
+            list = new ArrayList<>((Collection<?>)obj);
         }
-        return false;
+        return list;
+    }
+    private static final Map<Class<?>, Class<?>> WRAPPER_TYPE_MAP;
+    static {
+        WRAPPER_TYPE_MAP = new HashMap<Class<?>, Class<?>>(16);
+        WRAPPER_TYPE_MAP.put(Integer.class, int.class);
+        WRAPPER_TYPE_MAP.put(Byte.class, byte.class);
+        WRAPPER_TYPE_MAP.put(Character.class, char.class);
+        WRAPPER_TYPE_MAP.put(Boolean.class, boolean.class);
+        WRAPPER_TYPE_MAP.put(Double.class, double.class);
+        WRAPPER_TYPE_MAP.put(Float.class, float.class);
+        WRAPPER_TYPE_MAP.put(Long.class, long.class);
+        WRAPPER_TYPE_MAP.put(Short.class, short.class);
+        WRAPPER_TYPE_MAP.put(Void.class, void.class);
     }
 
-    public void Serialize(Object obj) throws NoSuchMethodException {
+    public String Serialize(Object obj,String folder) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, NoSuchFieldException {
 
-        Field[] fields=obj.getClass().getFields();
+        Field[] fields = obj.getClass().getDeclaredFields();
 
-        List<Field> AllFields=getFieldNames(fields);
+        String name=(String)obj.getClass().getDeclaredField("title").get(obj);
 
-        Map<String, Object> ForJson = new HashMap<>();
+        List<Field> allFields = List.of(fields);
 
-        for(Field field : AllFields){
+        Map<String, Object> forJson = new HashMap<>();
 
-            if(field.isAnnotationPresent(Const.class)){
-                if(isPrimitiveOrWrapper(field)){
-                    ForJson.put(field.getName(),field.get(obj));
+        for (Field field : allFields) {
+            if (field.isAnnotationPresent(Const.class)) {
+                if (isPrimitiveOrWrapper(field)) {
+                    forJson.put(field.getName(), field.get(obj));
                 }
-                else if(field.getType().isEnum()){
-                    ForJson.put(field.getName(),field.get(obj));
-                }
-                else {
-                    Method get=field.getType().getMethod("get");
-                    Method size=field.getType().getMethod("size");
-                    int Size=size.invoke(obj);
-                    for(int i=0;i< Size;i++){
-
+                else if(field.getType().getSimpleName().equals("List")){
+                    List<?> list=convertObjectToList(field.get(obj));
+                    List<Object> Names=new ArrayList<>();
+                    for(int i = 0; i<list.size(); i++) {
+                        if(WRAPPER_TYPE_MAP.containsKey(list.get(i).getClass())){
+                            Names.add(list.get(i));
+                        }
+                        else {
+                            Names.add(Serialize(list.get(i), folder + name + '/'));
+                        }
                     }
                 }
             }
-
         }
+        return name;
     }
 }
